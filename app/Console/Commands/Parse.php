79e601fa12a891c3aebf7129cc031e1796406adf
got26days <got26days@gmail.com>
2022-01-13 13:15:14 +0300

    Change

--- app/Console/Commands/Parse.php
+++ app/Console/Commands/Parse.php
@@ -87,7 +87,7 @@ class Parse extends Command
                     //     continue;
                     // }
 
-               
+
                     $badge = new Badge();
                     $badge->id = $row['Id'];
                     $badge->user_id = $row['UserId'];
@@ -100,175 +100,175 @@ class Parse extends Command
                     }
                     $badge->date = $row['Date'];
                     $badge->save();
-                
                 } catch (Exception $e) {
                     Log::info($e);
                 }
-        }
+            }
 
 
-        if ($table == 'comments') {
+            if ($table == 'comments') {
 
-            $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Comments.xml"));
+                $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Comments.xml"));
 
-            while ($row = $streamer->getNode()) {
+                while ($row = $streamer->getNode()) {
 
-                $comment = Comment::where(
-                    'id',
-                    $row['Id']
-                )->first();
-                if (!$comment) {
-                    $comment = new Comment();
-                    $comment->id = $row['Id'];
-                    $comment->post_id = $row['PostId'];
-                    $comment->user_id = $row['UserId'];
-                    $comment->score = $row['Score'];
-                    $comment->content_license = $row['ContentLicense'];
-                    $comment->user_display_name = $row['UserDisplayName'];
-                    $comment->text = $row['Text'];
-                    $comment->save();
+                    $comment = Comment::where(
+                        'id',
+                        $row['Id']
+                    )->first();
+                    if (!$comment) {
+                        $comment = new Comment();
+                        $comment->id = $row['Id'];
+                        $comment->post_id = $row['PostId'];
+                        $comment->user_id = $row['UserId'];
+                        $comment->score = $row['Score'];
+                        $comment->content_license = $row['ContentLicense'];
+                        $comment->user_display_name = $row['UserDisplayName'];
+                        $comment->text = $row['Text'];
+                        $comment->save();
+                    }
                 }
             }
-        }
-        if ($table == 'postHistory') {
-            $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/PostsHistory.xml"));
+            if ($table == 'postHistory') {
+                $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/PostsHistory.xml"));
 
-            while ($row = $streamer->getNode()) {
-                $row = simplexml_load_string($row);
-
-                dd($row);
-                try {
+                while ($row = $streamer->getNode()) {
                     $row = simplexml_load_string($row);
 
-                    if ($row['Id'] <= 17209927) {
-                        continue;
-                    }
+                    dd($row);
+                    try {
+                        $row = simplexml_load_string($row);
 
-                    $postHistory = new PostHistory();
-                    $postHistory->id = $row['Id'];
-                    $postHistory->post_id = $row['PostId'];
-                    $postHistory->user_id = $row['UserId'];
-                    $postHistory->post_history_type_id = $row['PostHistoryTypeId'];
-                    $postHistory->content_license = $row['ContentLicense'];
-                    $postHistory->revision_guid = $row['RevisionGuid'];
-                    $postHistory->text = $row['Text'];
-                    $postHistory->comment = $row['Comment'];
-                    $postHistory->save();
+                        if ($row['Id'] <= 17209927) {
+                            continue;
+                        }
 
-                    $this->info($postHistory->id);
-                } catch (Exception $e) {
-                    Log::info($e);
-                }
-            }
-        }
+                        $postHistory = new PostHistory();
+                        $postHistory->id = $row['Id'];
+                        $postHistory->post_id = $row['PostId'];
+                        $postHistory->user_id = $row['UserId'];
+                        $postHistory->post_history_type_id = $row['PostHistoryTypeId'];
+                        $postHistory->content_license = $row['ContentLicense'];
+                        $postHistory->revision_guid = $row['RevisionGuid'];
+                        $postHistory->text = $row['Text'];
+                        $postHistory->comment = $row['Comment'];
+                        $postHistory->save();
 
-        if ($table == 'postLinks') {
-            $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/PostLinks.xml"));
-
-            while ($row = $streamer->getNode()) {
-                try {
-                    $row = simplexml_load_string($row);
-
-                    if ($row['Id'] <= 1882290402) {
-                        continue;
-                    }
-
-                    $postLink = new PostLink();
-                    $postLink->id = $row['Id'];
-                    $postLink->related_post_id = $row['RelatedPostId'];
-                    $postLink->post_id = $row['PostId'];
-                    $postLink->link_type_id = $row['LinkTypeId'];
-                    $postLink->save();
-
-                    $this->info($postLink->id);
-                } catch (Exception $e) {
-                    Log::info($e);
-                }
-            }
-        }
-
-        if ($table == 'posts') {
-            $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Posts.xml"));
-
-            while ($row = $streamer->getNode()) {
-                $post = Post::where('id', $row['Id'])->first();
-                if (!$post) {
-                    $post = new Post();
-                    $post->id = $row['Id'];
-                    $post->owner_user_id = $row['OwnerUserId'];
-                    $post->last_editor_user_id = $row['LastEditorUserId'];
-                    $post->post_type_id = $row['PostTypeId'];
-                    $post->accepted_answer_id = $row['AcceptedAnswerId'];
-                    $post->score = $row['Score'];
-                    $post->parent_id = $row['ParentId'];
-                    $post->view_count = $row['ViewCount'];
-                    $post->answer_count = $row['AnswerCount'];
-                    $post->comment_count = $row['CommentCount'];
-                    $post->owner_display_name = $row['OwnerDisplayName'];
-                    $post->last_editor_display_name = $row['LastEditorDisplayName'];
-                    $post->title = $row['Title'];
-                    $post->tags = $row['Tags'];
-                    $post->content_license = $row['ContentLicense'];
-                    $post->body = $row['Body'];
-                    $post->favorite_count = $row['FavoriteCount'];
-                    $post->community_owned_date = $row['CommunityOwnedDate'];
-                    $post->closed_date = $row['ClosedDate'];
-                    $post->last_edit_date = $row['LastEditDate'];
-                    $post->last_activity_date = $row['LastActivityDate'];
-                    $post->save();
-                }
-            }
-        }
-
-
-
-        if ($table == 'users') {
-            $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Users.xml"));
-
-            while ($row = $streamer->getNode()) {
-                $user = User::where('id', $row['Id'])->first();
-                if (!$user) {
-                    // dd($row);
-                    if ($row['Id'] > 0) {
-                        $user = new User();
-                        $user->id = $row['Id'];
-                        $user->email = Str::random(10) . '@gmail.com';
-                        $user->password = bcrypt('password3');
-                        $user->account_id = $row['AccountId'];
-                        $user->reputation = $row['Reputation'];
-                        $user->views = $row['Views'];
-                        $user->down_votes = $row['DownVotes'];
-                        $user->up_votes = $row['UpVotes'];
-                        $user->display_name = $row['DisplayName'];
-                        $user->location = $row['Location'];
-                        $user->profile_image_url = $row['ProfileImageUrl'];
-                        $user->website_url = $row['WebsiteUrl'];
-                        $user->about_me = $row['AboutMe'];
-                        $user->last_access_date = $row['LastAccessDate'];
-                        $user->save();
+                        $this->info($postHistory->id);
+                    } catch (Exception $e) {
+                        Log::info($e);
                     }
                 }
             }
-        }
 
-        if ($table == 'votes') {
-            $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Votes.xml"));
+            if ($table == 'postLinks') {
+                $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/PostLinks.xml"));
 
-            while ($row = $streamer->getNode()) {
-                $vote = Vote::where('id', $row['Id'])->first();
-                if (!$vote) {
-                    // dd($row);
-                    $vote = new Vote();
-                    $vote->id = $row['Id'];
-                    $vote->user_id = $row['UserId'];
-                    $vote->post_id = $row['PostId'];
-                    $vote->vote_type_id = $row['VoteTypeId'];
-                    $vote->bounty_amount = $row['BountyAmount'];
-                    $vote->save();
+                while ($row = $streamer->getNode()) {
+                    try {
+                        $row = simplexml_load_string($row);
+
+                        if ($row['Id'] <= 1882290402) {
+                            continue;
+                        }
+
+                        $postLink = new PostLink();
+                        $postLink->id = $row['Id'];
+                        $postLink->related_post_id = $row['RelatedPostId'];
+                        $postLink->post_id = $row['PostId'];
+                        $postLink->link_type_id = $row['LinkTypeId'];
+                        $postLink->save();
+
+                        $this->info($postLink->id);
+                    } catch (Exception $e) {
+                        Log::info($e);
+                    }
                 }
             }
-        }
 
-        return 'Success';
+            if ($table == 'posts') {
+                $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Posts.xml"));
+
+                while ($row = $streamer->getNode()) {
+                    $post = Post::where('id', $row['Id'])->first();
+                    if (!$post) {
+                        $post = new Post();
+                        $post->id = $row['Id'];
+                        $post->owner_user_id = $row['OwnerUserId'];
+                        $post->last_editor_user_id = $row['LastEditorUserId'];
+                        $post->post_type_id = $row['PostTypeId'];
+                        $post->accepted_answer_id = $row['AcceptedAnswerId'];
+                        $post->score = $row['Score'];
+                        $post->parent_id = $row['ParentId'];
+                        $post->view_count = $row['ViewCount'];
+                        $post->answer_count = $row['AnswerCount'];
+                        $post->comment_count = $row['CommentCount'];
+                        $post->owner_display_name = $row['OwnerDisplayName'];
+                        $post->last_editor_display_name = $row['LastEditorDisplayName'];
+                        $post->title = $row['Title'];
+                        $post->tags = $row['Tags'];
+                        $post->content_license = $row['ContentLicense'];
+                        $post->body = $row['Body'];
+                        $post->favorite_count = $row['FavoriteCount'];
+                        $post->community_owned_date = $row['CommunityOwnedDate'];
+                        $post->closed_date = $row['ClosedDate'];
+                        $post->last_edit_date = $row['LastEditDate'];
+                        $post->last_activity_date = $row['LastActivityDate'];
+                        $post->save();
+                    }
+                }
+            }
+
+
+
+            if ($table == 'users') {
+                $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Users.xml"));
+
+                while ($row = $streamer->getNode()) {
+                    $user = User::where('id', $row['Id'])->first();
+                    if (!$user) {
+                        // dd($row);
+                        if ($row['Id'] > 0) {
+                            $user = new User();
+                            $user->id = $row['Id'];
+                            $user->email = Str::random(10) . '@gmail.com';
+                            $user->password = bcrypt('password3');
+                            $user->account_id = $row['AccountId'];
+                            $user->reputation = $row['Reputation'];
+                            $user->views = $row['Views'];
+                            $user->down_votes = $row['DownVotes'];
+                            $user->up_votes = $row['UpVotes'];
+                            $user->display_name = $row['DisplayName'];
+                            $user->location = $row['Location'];
+                            $user->profile_image_url = $row['ProfileImageUrl'];
+                            $user->website_url = $row['WebsiteUrl'];
+                            $user->about_me = $row['AboutMe'];
+                            $user->last_access_date = $row['LastAccessDate'];
+                            $user->save();
+                        }
+                    }
+                }
+            }
+
+            if ($table == 'votes') {
+                $streamer = \Prewk\XmlStringStreamer::createStringWalkerParser(public_path("database/Votes.xml"));
+
+                while ($row = $streamer->getNode()) {
+                    $vote = Vote::where('id', $row['Id'])->first();
+                    if (!$vote) {
+                        // dd($row);
+                        $vote = new Vote();
+                        $vote->id = $row['Id'];
+                        $vote->user_id = $row['UserId'];
+                        $vote->post_id = $row['PostId'];
+                        $vote->vote_type_id = $row['VoteTypeId'];
+                        $vote->bounty_amount = $row['BountyAmount'];
+                        $vote->save();
+                    }
+                }
+            }
+
+            return 'Success';
+        }
     }
 }
