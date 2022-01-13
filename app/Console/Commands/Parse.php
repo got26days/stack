b917a06005029e57811e8b0304a5ba049bcf0187
got26days <got26days@gmail.com>
2022-01-13 13:11:43 +0300

    Change parser

--- app/Console/Commands/Parse.php
+++ app/Console/Commands/Parse.php
@@ -80,6 +80,9 @@ class Parse extends Command
 
             while ($row = $streamer->getNode()) {
 
+                $row = simplexml_load_string($row);
+                dd($row);
+
                 $badge = Badge::where('id', $row['Id'])->first();
                 if (!$badge) {
                     $badge = new Badge();
@@ -93,6 +96,8 @@ class Parse extends Command
                         $badge->tag_based = 1;
                     }
                     $badge->date = $row['Date'];
+                    $badge->created_at = $row['CreatedDate'];
+                    $badge->updated_at = $row['CreatedDate'];
                     $badge->save();
                 }
             }
