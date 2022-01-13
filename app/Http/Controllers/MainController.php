df574c926856bd631387474fa84863c95f4fb285
got26days <got26days@gmail.com>
2022-01-13 07:24:31 +0300

    Test

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,6 +18,6 @@ class MainController extends Controller
     public function index()
     {
 
-        return PostHistory::latest()->limit(10)->get();
+        return PostHistory::latest()->first();
     }
 }
