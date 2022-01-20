ce1cfbcd59442a32852e3230b1d813757fc54ac1
got26days <got26days@gmail.com>
2022-01-20 11:14:29 +0300

    Test where clause

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -24,10 +24,9 @@ class MainController extends Controller
 
     public function test()
     {
-        dd(3);
-        $posts = Post::latest()->paginate(20);
-        // ->where('tags', '<php>')
-        // ->latest()->paginate(20);
+        $posts = Post::where('post_type_id', 1)
+            ->where('tags', '<php>')
+            ->latest()->paginate(20);
 
         return $posts;
     }
