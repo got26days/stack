c6c256d23aa27243d189bda0dbec3848957fd1c6
got26days <got26days@gmail.com>
2022-01-27 20:46:07 +0300

    Return take

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,7 +60,7 @@ class PostController extends Controller
                 $posts = $posts->where('closed_date', null)->latest();
             }
 
-            return $posts->paginate(20);
+            return $posts->take(8000000)->paginate(20);
         });
 
 
