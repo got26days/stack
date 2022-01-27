cf0327c10c425c90a95f4683aa7b90cd94208682
got26days <got26days@gmail.com>
2022-01-27 21:20:05 +0300

    new pivot table

---
+++ app/Http/Controllers/PostTagSecondController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostTagSecond;
+use App\Http\Requests\StorePostTagSecondRequest;
+use App\Http\Requests\UpdatePostTagSecondRequest;
+
+class PostTagSecondController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostTagSecondRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostTagSecondRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PostTagSecond  $postTagSecond
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PostTagSecond $postTagSecond)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PostTagSecond  $postTagSecond
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PostTagSecond $postTagSecond)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostTagSecondRequest  $request
+     * @param  \App\Models\PostTagSecond  $postTagSecond
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostTagSecondRequest $request, PostTagSecond $postTagSecond)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PostTagSecond  $postTagSecond
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PostTagSecond $postTagSecond)
+    {
+        //
+    }
+}
