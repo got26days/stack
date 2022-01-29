74bedf4689b1c09a34ff67a2bd74899c1cb77d36
got26days <got26days@gmail.com>
2022-01-29 14:12:00 +0300

    Change table to model

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     use HasFactory;
+
+    protected $appends = ['tagsArray'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'owner_user_id', 'id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
+
+    public function getTagsArrayAttribute()
+    {
+
+        $tagsArray = [];
+
+
+        if ($this->tags) {
+            $array = explode('>', $this->tags);
+
+            foreach ($array as $item) {
+                if ($item != '') {
+                    $tagsArray[] = str_replace("<", "", $item);
+                }
+            }
+        }
+        return $tagsArray;
+    }
 }
