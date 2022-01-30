96e99253f158e687fba8e47bd602375e66b17d0f
got26days <got26days@gmail.com>
2022-01-30 21:43:11 +0300

    Change date

--- app/Console/Commands/CachePosts.php
+++ app/Console/Commands/CachePosts.php
@@ -102,7 +102,7 @@ class CachePosts extends Command
             ->where('id', '!=', 237745)
             ->where('id', '!=', 237748)
             ->where('id', '!=', 237757)
-            ->where('id', '>', 337757)
+            ->where('id', '>', 537757)
             ->chunkById(
                 10000,
                 function ($posts) {
