37b3da32ae61022ecb681c8feb8876579d29e5b1
got26days <got26days@gmail.com>
2022-02-08 17:07:24 +0300

    Change enrty

--- app/Http/Controllers/Admin/TagCrudController.php
+++ app/Http/Controllers/Admin/TagCrudController.php
@@ -67,7 +67,7 @@ class TagCrudController extends CrudController
             'label' => "Answers",
             'type' => 'closure',
             'function' => function ($entry) {
-                return ($entry->tagsRelationship()->count() + $entry->tagsRelationshipSecond()->count()) * 2;
+                return ($entry->tagsRelationship()->count() * 2 + $entry->tagsRelationshipSecond()->count() * 2);
             }
         ]);
 
